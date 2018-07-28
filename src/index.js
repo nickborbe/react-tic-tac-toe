ec248046e4ac12ef8f871a278a575c57cf0d24de
nickborbe <nickborbe@gmail.com>
2018-07-28 16:58:50 -0400

    dope tic tac toe

--- src/index.js
+++ src/index.js
@@ -19,43 +19,28 @@ class Square extends React.Component {
 
   
   class Board extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        }
-    }
-
-    putAnXorO(i){
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext? 'X' : 'O'
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //         squares: Array(9).fill(null),
+    //         xIsNext: true,
+    //     }
+    // }
 
 
     renderSquare(i) {
-      return <Square blah={this.state.squares[i]}
-      onClick={() => this.putAnXorO(i)}
+      return <Square blah={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
       />;
     }
   
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if(winner){
-            status = `Winner: ${winner}`
-        }else{
-         status = `Next player: ${(this.state.xIsNext? 'X' : 'O')}`;
-        }
+        // const winner = calculateWinner(this.state.squares);
+      
 
   
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -77,15 +62,101 @@ class Square extends React.Component {
   }
   
   class Game extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            history: [{squares: Array(9).fill(null)}],
+            xIsNext: true,
+            stepNumber: 0,
+        }
+    }
+
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
+
+
+    putAnXorO(i){
+        if(this.state.stepNumber !== this.state.history.length - 1){
+            return;
+        }
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if(calculateWinner(squares) || squares[i]){
+            return
+        }
+        squares[i] = this.state.xIsNext? 'X' : 'O'
+        this.setState({
+            history: this.state.history.concat([{
+                squares: squares
+            }]),
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+        });
+    }
+
+    resetTheGame(){
+        this.setState({
+            history: [{squares: Array(9).fill(null)}],
+            xIsNext: true,
+            stepNumber: 0,
+        })
+    }
+
+    showTheResetButton(){
+        if(calculateWinner(this.state.history[this.state.history.length - 1].squares) || this.state.history.length === 10){
+            return(
+            <button onClick={()=> this.resetTheGame()}> 
+                Reset Game
+            </button>
+            );
+        }
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move)=>{
+            const desc = move ? `Go to move # ${move}` : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={()=> this.jumpTo(move)}>
+                        {desc}
+                    </button>
+                </li>
+            )
+        });
+
+
+
+        let status;
+        if(winner){
+            status = `Winner: ${winner}`
+        }else{
+         status = `Next player: ${(this.state.xIsNext? 'X' : 'O')}`;
+        }
+
+
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+                {this.showTheResetButton()}
+            <Board
+            squares = {current.squares}
+            onClick = {(i)=> this.putAnXorO(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
